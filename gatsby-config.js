62545a4237e1f463c60759f13f8b2ade0378d01a
minesp3164 <lhd000721@gmail.com>
2024-12-15 17:57:41 +0900

    remove package-lock.json

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,9 +7,6 @@ const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
  */
 module.exports = {
   siteMetadata: {
-    // You can overwrite values here that are used for the SEO component
-    // You can also add new values here to query them like usual
-    // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-minimal-blog/gatsby-config.js
     siteTitle: `Minesp's blog`,
     siteTitleAlt: `Minesp's blog`,
     siteHeadline: `Minesp's blog `,
